9e18ec41e4db47fec011df435858825524a02edd
nuts <cngjsskaisme@gmail.com>
2023-06-06 23:03:18 +0900

    feat: added typescript, prisma and pinia

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import VideoList from './components/VideoList.vue'
@@ -13,6 +14,9 @@ const router = createRouter({
   routes,
 })
 
+const pinia = createPinia()
+
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
